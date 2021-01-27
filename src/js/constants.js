1f9a70d490fc0a8a6160b1624ee3d38947655fcc
Sebastian <sebastian@sanctuary.computer>
2021-01-27 14:34:56 -0500

    Adds backgorund.js

---
+++ src/js/constants.js
@@ -0,0 +1,14 @@
+export const Theme = {
+  GOOD: 'good',
+  EVIL: 'evil',
+};
+
+export const Operator = {
+  ADD: '+=',
+  SUBTRACT: '-=',
+};
+
+export const Direction = {
+  UP: 'up',
+  DOWN: 'down',
+};
