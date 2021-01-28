ad55ea7db473c6f24138d9ceea710fe4009e77f0
Sebastian Odell <sebastian@sanctuary.computer>
2021-01-28 13:08:31 -0500

    Background transition (#30)
    * Adds gsap
    
    * Cleans up styles
    
    * Adds backgorund.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,10 @@
 import * as PIXI from 'pixi.js';
+import gsap from 'gsap';
+
+import { Direction } from './constants';
 
 import goodBg from '/assets/images/bg-good.png';
-// import evilBg from '/assets/images/bg-evil.png';
+import evilBg from '/assets/images/bg-evil.png';
 
 const clouds = 'https://res.cloudinary.com/dvxikybyi/image/upload/v1486634113/2yYayZk_vqsyzx.png';
 
@@ -15,13 +18,16 @@ export default (function () {
         autoResize: true,
       }),
       stage: null,
-      texture: null,
-      background: null,
+      textureGood: null,
+      textureEvil: null,
+      backgroundGood: null,
+      backgroundEvil: null,
       displacementSprite: null,
       displacementFilter: null,
     },
     state: {
       count: 0,
+      direction: Direction.UP,
     },
     elements: {
       background: document.getElementById('Background'),
@@ -34,7 +40,11 @@ export default (function () {
       Background.pixi.displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
 
       Background.pixi.stage.addChild(Background.pixi.displacementSprite);
-      Background.pixi.stage.addChild(Background.pixi.background);
+      Background.pixi.stage.addChild(Background.pixi.backgroundEvil);
+      Background.pixi.stage.addChild(Background.pixi.backgroundGood);
+
+      Background.pixi.displacementSprite.scale.set(0.5);
+      Background.pixi.displacementFilter.scale.set(20);
 
       window.addEventListener('resize', Background.resize);
 
@@ -43,8 +53,10 @@ export default (function () {
 
     setupPixi() {
       Background.pixi.stage = new PIXI.Container();
-      Background.pixi.texture = PIXI.Texture.from(goodBg);
-      Background.pixi.background = new PIXI.Sprite(Background.pixi.texture);
+      Background.pixi.textureGood = PIXI.Texture.from(goodBg);
+      Background.pixi.textureEvil = PIXI.Texture.from(evilBg);
+      Background.pixi.backgroundGood = new PIXI.Sprite(Background.pixi.textureGood);
+      Background.pixi.backgroundEvil = new PIXI.Sprite(Background.pixi.textureEvil);
       Background.pixi.displacementSprite = PIXI.Sprite.from(clouds);
       Background.pixi.displacementFilter = new PIXI.filters.DisplacementFilter(
         Background.pixi.displacementSprite
@@ -57,14 +69,52 @@ export default (function () {
       Background.pixi.displacementSprite.x = Background.state.count * 10;
       Background.pixi.displacementSprite.y = Background.state.count * 10;
 
-      Background.state.count += 0.05;
-
+      Background.updateCountDirection();
+      Background.updateCount();
       Background.pixi.stage.filters = [Background.pixi.displacementFilter];
       Background.pixi.app.renderer.render(Background.pixi.stage);
     },
 
+    updateCountDirection() {
+      if (Background.state.count >= 100) {
+        Background.state.direction = Direction.DOWN;
+      }
+
+      if (Background.state.count <= 0) {
+        Background.state.direction = Direction.UP;
+      }
+    },
+
+    updateCount() {
+      if (Background.state.direction === Direction.UP) {
+        Background.state.count += 0.05;
+      } else {
+        Background.state.count -= 0.05;
+      }
+    },
+
     resize() {
       Background.pixi.app.renderer.resize(window.innerWidth, window.innerHeight);
+    },
+
+    transitionSlide(slideIndex, direction) {
+      gsap.to(Background.pixi.stage.children, {
+        duration: (1000 * 2) / 1000,
+        alpha: 0,
+        ease: 'power3.out',
+      });
+
+      gsap.to(Background.pixi.displacementSprite, {
+        duration: (1000 * 2) / 1000,
+        rotation: `${direction + 0.5}`,
+        ease: 'power3.out',
+      });
+
+      gsap.to(Background.pixi.stage.children[slideIndex], {
+        duration: (1000 * 2) / 1000,
+        alpha: 1,
+        ease: 'power3.out',
+      });
     },
   };
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
 'use strict';
 import background from './background';
 import audio from './audio';
-
-const GOOD = 'good';
-const EVIL = 'evil';
+import { Operator, Theme } from './constants';
 
 const themeToggle = document.querySelector('[data-trigger="theme-toggle"]');
 
 const handleToggleTheme = function () {
   const activeTheme = document.body.getAttribute('data-theme');
 
-  if (activeTheme === GOOD) {
-    document.body.setAttribute('data-theme', EVIL);
+  if (activeTheme === Theme.GOOD) {
+    document.body.setAttribute('data-theme', Theme.EVIL);
     audio.makeEvil();
+    background.transitionSlide(1, Operator.ADD);
   } else {
-    document.body.setAttribute('data-theme', GOOD);
+    document.body.setAttribute('data-theme', Theme.GOOD);
     audio.makeGood();
+    background.transitionSlide(2, Operator.SUBTRACT);
   }
 };
 
