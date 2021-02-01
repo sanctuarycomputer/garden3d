3918b8c2d53a034d125d4e93caf32d1574b768c2
Sebastian Odell <sebastian@sanctuary.computer>
2021-02-01 12:07:38 -0500

    updates background.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,10 @@
 import * as PIXI from 'pixi.js';
 import gsap from 'gsap';
 
-import { Direction } from './constants';
+import { Direction, Theme, Operator } from './constants';
 
-import goodBg from '/assets/images/bg-good.png';
-import evilBg from '/assets/images/bg-evil.png';
+import goodBg from '/assets/images/bg-good.jpg';
+import evilBg from '/assets/images/bg-evil.jpg';
 
 const clouds = 'https://res.cloudinary.com/dvxikybyi/image/upload/v1486634113/2yYayZk_vqsyzx.png';
 
@@ -24,9 +24,12 @@ export default (function () {
       backgroundEvil: null,
       displacementSprite: null,
       displacementFilter: null,
+      transitionSprite: null,
+      transitionFilter: null,
     },
     state: {
       count: 0,
+      activeTheme: Theme.GOOD,
       direction: Direction.UP,
     },
     elements: {
@@ -35,19 +38,7 @@ export default (function () {
 
     init() {
       Background.setupPixi();
-
-      Background.elements.background.appendChild(Background.pixi.app.view);
-      Background.pixi.displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-
-      Background.pixi.stage.addChild(Background.pixi.displacementSprite);
-      Background.pixi.stage.addChild(Background.pixi.backgroundEvil);
-      Background.pixi.stage.addChild(Background.pixi.backgroundGood);
-
-      Background.pixi.displacementSprite.scale.set(0.5);
-      Background.pixi.displacementFilter.scale.set(20);
-
-      window.addEventListener('resize', Background.resize);
-
+      Background.setupLayers();
       Background.animate();
     },
 
@@ -57,39 +48,60 @@ export default (function () {
       Background.pixi.textureEvil = PIXI.Texture.from(evilBg);
       Background.pixi.backgroundGood = new PIXI.Sprite(Background.pixi.textureGood);
       Background.pixi.backgroundEvil = new PIXI.Sprite(Background.pixi.textureEvil);
+
       Background.pixi.displacementSprite = PIXI.Sprite.from(clouds);
       Background.pixi.displacementFilter = new PIXI.filters.DisplacementFilter(
         Background.pixi.displacementSprite
       );
+      Background.pixi.displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+
+      Background.pixi.transitionSprite = PIXI.Sprite.from(clouds);
+      Background.pixi.transitionFilter = new PIXI.filters.DisplacementFilter(
+        Background.pixi.transitionSprite
+      );
+      Background.pixi.transitionSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+    },
+
+    setupLayers() {
+      Background.elements.background.appendChild(Background.pixi.app.view);
+      Background.pixi.stage.addChild(Background.pixi.displacementSprite);
+      Background.pixi.stage.addChild(Background.pixi.backgroundEvil);
+      Background.pixi.stage.addChild(Background.pixi.backgroundGood);
+      Background.pixi.stage.addChild(Background.pixi.transitionSprite);
+
+      window.addEventListener('resize', Background.resize);
     },
 
     animate() {
       requestAnimationFrame(Background.animate);
 
+      if (Background.state.activeTheme === Theme.GOOD) {
+        Background.pixi.displacementSprite.scale.set(0.5);
+        Background.pixi.displacementFilter.scale.set(20);
+        Background.pixi.transitionSprite.scale.set(0.5);
+        Background.pixi.transitionFilter.scale.set(20);
+      } else {
+        Background.pixi.displacementSprite.scale.set(0.5);
+        Background.pixi.displacementFilter.scale.set(50);
+        Background.pixi.transitionSprite.scale.set(0.5);
+        Background.pixi.transitionFilter.scale.set(50);
+      }
       Background.pixi.displacementSprite.x = Background.state.count * 10;
       Background.pixi.displacementSprite.y = Background.state.count * 10;
 
-      Background.updateCountDirection();
       Background.updateCount();
-      Background.pixi.stage.filters = [Background.pixi.displacementFilter];
+      Background.pixi.stage.filters = [
+        Background.pixi.displacementFilter,
+        Background.pixi.transitionFilter,
+      ];
       Background.pixi.app.renderer.render(Background.pixi.stage);
-    },
-
-    updateCountDirection() {
-      if (Background.state.count >= 100) {
-        Background.state.direction = Direction.DOWN;
-      }
-
-      if (Background.state.count <= 0) {
-        Background.state.direction = Direction.UP;
-      }
     },
 
     updateCount() {
       if (Background.state.direction === Direction.UP) {
-        Background.state.count += 0.05;
-      } else {
         Background.state.count -= 0.05;
+      } else {
+        Background.state.count += 0.05;
       }
     },
 
@@ -97,21 +109,33 @@ export default (function () {
       Background.pixi.app.renderer.resize(window.innerWidth, window.innerHeight);
     },
 
+    makeGood() {
+      Background.state.activeTheme = Theme.GOOD;
+      Background.state.direction = Direction.UP;
+      Background.transitionSlide(2, Operator.SUBTRACT);
+    },
+
+    makeEvil() {
+      Background.state.activeTheme = Theme.EVIL;
+      Background.state.direction = Direction.DOWN;
+      Background.transitionSlide(1, Operator.ADD);
+    },
+
     transitionSlide(slideIndex, direction) {
       gsap.to(Background.pixi.stage.children, {
-        duration: (1000 * 2) / 1000,
+        duration: (500 * 2) / 1000,
         alpha: 0,
         ease: 'power3.out',
       });
 
-      gsap.to(Background.pixi.displacementSprite, {
+      gsap.to(Background.pixi.transitionSprite, {
         duration: (1000 * 2) / 1000,
-        rotation: `${direction + 0.5}`,
+        rotation: `${direction + 0.2}`,
         ease: 'power3.out',
       });
 
       gsap.to(Background.pixi.stage.children[slideIndex], {
-        duration: (1000 * 2) / 1000,
+        duration: (500 * 2) / 1000,
         alpha: 1,
         ease: 'power3.out',
       });
