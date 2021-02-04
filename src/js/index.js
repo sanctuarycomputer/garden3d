3e584c9ddf7b89266003d83084a7db9a254f3965
Sebastian Odell <sebastian@sanctuary.computer>
2021-02-04 08:32:19 -0500

    Shimmer link (#37)
    * stash
    
    * Adds shimmer link
    
    * clenaup

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 import background from './background';
 import audio from './audio';
+import shimmer from './shimmer';
+
 import { Theme } from './constants';
 
 const themeToggle = document.querySelector('[data-trigger="theme-toggle"]');
@@ -10,10 +12,12 @@ const handleToggleTheme = function () {
 
   if (activeTheme === Theme.GOOD) {
     document.body.setAttribute('data-theme', Theme.EVIL);
+    shimmer.makeEvil();
     audio.makeEvil();
     background.makeEvil();
   } else {
     document.body.setAttribute('data-theme', Theme.GOOD);
+    shimmer.makeGood();
     audio.makeGood();
     background.makeGood();
   }
