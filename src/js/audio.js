db47b33a704b4d6645e8e7b22ba30aa0a2752fc5
Sebastian Odell <sebastian@sanctuary.computer>
2021-02-08 15:45:30 -0500

    first pass gen music

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,49 +1,95 @@
+import activate from '@generative-music/piece-enough';
+import getSamples from '@generative-music/samples-alex-bainter'; // you may need to `npm link` this from your local directory.
+import createWebProvider from '@generative-music/web-provider';
+import createWebLibrary from '@generative-music/web-library';
+import * as Tone from 'tone';
+
 export default (function () {
-  const audioEl = document.getElementById('Audio');
-  const audioTriggers = document.querySelectorAll('[data-trigger="audio"]');
-  const goodSrc = document.getElementById('Audio--good').getAttribute('src');
-  const evilSrc = document.getElementById('Audio--evil').getAttribute('src');
-
-  const play = function () {
-    audioEl.play();
-    audioTriggers.forEach((audioTrigger) => {
-      audioTrigger.classList.add('AudioIcon--active');
-    });
-  };
-
-  const pause = function () {
-    audioEl.pause();
-    audioTriggers.forEach((audioTrigger) => {
-      audioTrigger.classList.remove('AudioIcon--active');
-    });
-  };
-
-  const handler = function () {
-    audioEl.paused ? play() : pause();
-  };
-
-  const audio = {
-    makeEvil: function () {
-      const wasPaused = audioEl.paused;
-
-      audioEl.setAttribute('src', evilSrc);
-      wasPaused ? pause() : play();
+  const Audio = {
+    piece: {
+      deactivate: null,
+      schedule: null,
+      effect: null,
+      distortion: null,
     },
-    makeGood: function () {
-      const wasPaused = audioEl.paused;
+    state: {
+      isLoaded: false,
+      isPlaying: false,
+    },
+    elements: {
+      audioTriggers: document.querySelectorAll('[data-trigger="audio"]'),
+    },
 
-      audioEl.setAttribute('src', goodSrc);
-      wasPaused ? pause() : play();
+    /**
+     * Private
+     */
+
+    _init() {
+      Audio._makePiece();
+      Audio._addEventListeners();
+    },
+
+    _addEventListeners() {
+      Audio.elements.audioTriggers.forEach((audioTrigger) => {
+        audioTrigger.addEventListener('click', Audio._handler);
+      });
+    },
+
+    _makePiece() {
+      const sampleIndex = getSamples({ format: 'wav', host: 'http://localhost:6969' });
+      const provider = createWebProvider();
+      const sampleLibrary = createWebLibrary({ sampleIndex, provider });
+
+      const effect = new Tone.PitchShift({
+        wet: 0,
+        pitch: -12,
+      }).toDestination();
+
+      activate({
+        sampleLibrary,
+        context: Tone.context,
+        destination: effect,
+      }).then(([deactivate, schedule]) => {
+        Audio.piece.deactivate = deactivate;
+        Audio.piece.schedule = schedule();
+        Audio.piece.effect = effect;
+        Audio.state.isLoaded = true;
+      });
+    },
+
+    _play() {
+      Tone.Transport.start();
+      Tone.start();
+      Audio.elements.audioTriggers.forEach((audioTrigger) => {
+        audioTrigger.classList.add('AudioIcon--active');
+      });
+    },
+
+    _pause() {
+      Tone.Transport.pause();
+      Audio.elements.audioTriggers.forEach((audioTrigger) => {
+        audioTrigger.classList.remove('AudioIcon--active');
+      });
+    },
+
+    _handler() {
+      Tone.Transport.state === 'started' ? Audio._pause() : Audio._play();
+    },
+
+    /**
+     * Public
+     */
+
+    makeEvil() {
+      Audio.piece.effect.wet.value = 0.8;
+    },
+
+    makeGood() {
+      Audio.piece.effect.wet.value = 0;
     },
   };
 
-  if (!audioEl.getAttribute('src')) {
-    audioEl.setAttribute('src', goodSrc);
-  }
+  Audio._init();
 
-  audioTriggers.forEach((audioTrigger) => {
-    audioTrigger.addEventListener('click', handler);
-  });
-
-  return audio;
+  return Audio;
 })();
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 import background from './background';
 import audio from './audio';
 import shimmer from './shimmer';
