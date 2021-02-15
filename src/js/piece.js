55625030e3d9208603ec4edd08c0ee54b7ff88c8
Sebastian <sebastian@sanctuary.computer>
2021-02-15 13:01:48 -0500

    Adds piece

--- src/js/piece.js
+++ src/js/piece.js
@@ -1,13 +1,9 @@
-// import activate from '@generative-music/piece-enough';
-// import getSamples from '@generative-music/samples-alex-bainter'; // you may need to `npm link` this from your local directory.
-// import createWebProvider from '@generative-music/web-provider';
-// import createWebLibrary from '@generative-music/web-library';
 import * as Tone from 'tone';
 import { getRandomNumberBetween, toss, invert, major9th } from '@generative-music/utilities';
 
 import Harp from './samples';
 
-const OCTAVES = [1, 2, 3];
+const OCTAVES = [4, 5, 6];
 const MIN_REPEAT_S = 2;
 const MAX_REPEAT_S = 20;
 const NOTES = toss(invert(major9th('Db'), 1), OCTAVES);
@@ -15,8 +11,11 @@ const NOTES = toss(invert(major9th('Db'), 1), OCTAVES);
 export default (function () {
   const Audio = {
     sampler: null,
+    filter: null,
+    distortion: null,
     state: {
       isLoaded: false,
+      isPlaying: false,
     },
 
     /**
@@ -24,10 +23,13 @@ export default (function () {
      */
 
     _init() {
+      Audio._setup();
+      Audio._createSampler();
+    },
+
+    _setup() {
       window.generativeMusic = window.generativeMusic || {};
       window.generativeMusic.rng = window.generativeMusic.rng || Math.random;
-
-      Audio._createSampler();
     },
 
     _createSampler() {
@@ -35,12 +37,21 @@ export default (function () {
     },
 
     _onSamplerLoaded() {
-      console.log('loaded');
       Audio.state.isLoaded = true;
-      Audio.play();
+      Audio._setupEffect();
+      Audio._generatePiece();
     },
 
-    play() {
+    _setupEffect() {
+      Audio.filter = new Tone.AutoFilter({
+        frequency: 50,
+        octave: 8,
+        wet: 1,
+      });
+      Audio.sampler.chain(Audio.filter, Tone.Destination);
+    },
+
+    _generatePiece() {
       if (Audio.state.isLoaded) {
         NOTES.forEach((note) => {
           const interval = getRandomNumberBetween(MIN_REPEAT_S, MAX_REPEAT_S);
@@ -53,9 +64,31 @@ export default (function () {
       }
     },
 
+    /**
+     * Public
+     */
+
+    toggleEffectOn() {
+      Audio.filter.start(0);
+    },
+
+    toggleEffectOff() {
+      Audio.filter.stop(0);
+    },
+
     start() {
+      Audio.state.isPlaying = true;
       Tone.Transport.start();
       Tone.start();
+    },
+
+    stop() {
+      Audio.state.isPlaying = false;
+      Tone.Transport.pause();
+    },
+
+    isPlaying() {
+      return Audio.state.isPlaying;
     },
   };
 
