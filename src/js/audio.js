ad4b340a91599be3049431b82ad97e256bea2a61
metalex9 <alexbainter@gmail.com>
2021-04-07 17:30:28 -0500

    Implement generative audio

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,49 +1,72 @@
-export default (function () {
-  const audioEl = document.getElementById('Audio');
-  const audioTriggers = document.querySelectorAll('[data-trigger="audio"]');
-  const goodSrc = document.getElementById('Audio--good').getAttribute('src');
-  const evilSrc = document.getElementById('Audio--evil').getAttribute('src');
+import generativeAudioPlayerFactory from './generative-audio-player-factory';
 
-  const play = function () {
-    audioEl.play();
+export default (function () {
+  const audioTriggers = document.querySelectorAll('[data-trigger="audio"]');
+  /*
+   * On iOS, if the physical ringer switch is set to silent, Web Audio API sound
+   * is muted unless WebKit detects some media element is playing. This will play
+   * a silent audio file to prevent WebKit from muting Web Audio API sound.
+   */
+  const silentAudio = document.getElementById('Audio--silence');
+
+  let player;
+  const ready = generativeAudioPlayerFactory
+    .prepareAndCreatePlayer()
+    .then((generativeAudioPlayer) => {
+      player = generativeAudioPlayer;
+      audioTriggers.forEach((audioTrigger) => {
+        audioTrigger.classList.add('AudioIcon--is-ready');
+        audioTrigger.addEventListener('click', handler);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  const whenReady = function (callback) {
+    return async function (...args) {
+      if (!player) {
+        await ready;
+      }
+      return callback(...args);
+    };
+  };
+
+  const unmute = function () {
+    player.unmute();
     audioTriggers.forEach((audioTrigger) => {
       audioTrigger.classList.add('AudioIcon--active');
     });
   };
 
-  const pause = function () {
-    audioEl.pause();
+  const mute = whenReady(function () {
+    player.mute();
     audioTriggers.forEach((audioTrigger) => {
       audioTrigger.classList.remove('AudioIcon--active');
     });
-  };
+  });
 
-  const handler = function () {
-    audioEl.paused ? play() : pause();
-  };
+  const handler = whenReady(async function () {
+    if (!player.isStarted()) {
+      await Promise.all([generativeAudioPlayerFactory.enablePlayback(), silentAudio.play()]);
+      player.start();
+    }
+    if (player.isMuted()) {
+      unmute();
+    } else {
+      mute();
+    }
+  });
 
   const audio = {
-    makeEvil: function () {
-      const wasPaused = audioEl.paused;
-
-      audioEl.setAttribute('src', evilSrc);
-      wasPaused ? pause() : play();
-    },
-    makeGood: function () {
-      const wasPaused = audioEl.paused;
-
-      audioEl.setAttribute('src', goodSrc);
-      wasPaused ? pause() : play();
-    },
+    makeEvil: whenReady(function () {
+      player.makeDark();
+    }),
+    makeGood: whenReady(function () {
+      player.makeLight();
+    }),
+    ready,
   };
-
-  if (!audioEl.getAttribute('src')) {
-    audioEl.setAttribute('src', goodSrc);
-  }
-
-  audioTriggers.forEach((audioTrigger) => {
-    audioTrigger.addEventListener('click', handler);
-  });
 
   return audio;
 })();
