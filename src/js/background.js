e0e6b1b7361036acb61346229e52b51a1554bf6c
Sebastian <sebastian@sanctuary.computer>
2021-08-12 13:55:17 -0400

    Updates background

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,7 @@
 import * as PIXI from 'pixi.js';
 import gsap from 'gsap';
 
-import { Direction, Theme, Operator } from './constants';
+import { Direction, Theme, Operator, Color } from './constants';
 
 import goodBg from '/assets/images/bg-good.jpg';
 import evilBg from '/assets/images/bg-evil.jpg';
@@ -168,29 +168,18 @@ export default (function () {
     },
 
     _fadeIn() {
-      gsap.to('#Background', {
-        duration: 2,
-        opacity: 1,
-        rotate: 0.01,
-        ease: 'power3.out',
-      });
+      const root = document.documentElement;
+      const Background = document.getElementById('Background');
 
-      gsap.to('#Hero', {
-        duration: 1,
-        delay: 1.5,
-        opacity: 1,
-        rotate: 0.01,
-        ease: 'power3.out',
-      });
+      Background.classList.add('Background--active');
 
-      gsap.to('#Content', {
-        duration: 1,
-        delay: 2,
-        opacity: 1,
-        y: 0,
-        rotate: 0.01,
-        ease: 'power3.out',
-      });
+      /**
+       * This setTimeout waits a couple seconds for the background animation to fade in
+       * before setting the initial --background-color variable to the default theme color (Good)
+       */
+      setTimeout(() => {
+        root.style.setProperty('--background-color', Color.GOOD);
+      }, 2000);
     },
 
     /**
